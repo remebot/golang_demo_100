91ba02817d65f0163cbb0ee0d61a0a42de37bad3
gao <mercury1259@outlook.com>
2025-09-05 16:34:52 +0800

    20250905 1635

---
+++ basic/basicdatatype/go_struct.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type Person struct {
+	Name    string
+	Age     int
+	Gender  int
+	Married bool
+}
+
+// 定义一个“成员函数”，方法接收者是 Person
+func (p Person) SayHello() {
+	fmt.Printf("大家好，我叫%s，我今年%d岁。\n", p.Name, p.Age)
+}
+
+// 定义一个带返回值的方法
+func (p *Person) IsAdult() bool {
+	return p.Age >= 18
+}
+
+// 定义一个指针接收者的方法，可以修改结构体内容
+func (p *Person) GrowUp() {
+	p.Age += 1
+}
+
+func main() {
+	p := Person{
+		Name:    "Bob",
+		Age:     20,
+		Gender:  1,
+		Married: true,
+	}
+	// 访问 Name 字段
+	fmt.Println(p.Name)
+	// 修改 Age 字段
+	p.Age = 21
+	fmt.Println(p.Name + "今年" + strconv.Itoa(p.Age) + "岁了")
+
+	// 使用成员函数
+	p.SayHello()
+	isAdult := p.IsAdult()
+	if isAdult {
+		fmt.Println(p.Name + "成年了")
+	} else {
+		fmt.Println(p.Name + "未成年")
+	}
+	p.GrowUp()
+	fmt.Printf("生日过后，我 %d 岁啦\n", p.Age)
+
+}
--- basic/basicdatatype/inputoutput.go
+++ basic/basicdatatype/inputoutput.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func main1() {
 
 	//fmt.Print("a")
 	//fmt.Print("b")
@@ -15,9 +15,31 @@ func main() {
 	//
 	//fmt.Printf("a=%d,g=%f,n=%t", a, g, n)
 
-	k := 2.171241
-	fmt.Printf("k=%.2f", k)
+	//k := 2.171241
+	//fmt.Printf("k=%.2f", k)
+	//
+	//k2 := 2.17999
+	//fmt.Printf("k2=%.2f", k2)
+	//
+	//fmt.Println("")
+	////const A, B, C = 1, 2, 3
+	////fmt.Println(A, B, C)
+	//const (
+	//	A = 1
+	//	B = 2
+	//	C = 3
+	//)
+	//fmt.Println(A, B, C)
 
-	k2 := 2.17999
-	fmt.Printf("k2=%.2f", k2)
+	// 定义星期枚举
+	const (
+		Monday    = iota + 1 // 1
+		Tuesday              // 2
+		Wednesday            // 3
+		Thursday             // 4
+		Friday               // 5
+		Saturday             // 6
+		Sunday               // 7
+	)
+	fmt.Println(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)
 }
